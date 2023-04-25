3fa2b81e822be501a246d0ffea47516db5e55159
UrijHoruzij <63039080+UrijHoruzij@users.noreply.github.com>
2023-04-25 12:35:53 +0000

    Deploying to main from @ UrijHoruzij/todo-app@c95ad73263b7046a656faed1071931b0c6e82841 🚀

--- src/components/Context/index.ts
+++
@@ -1,5 +0,0 @@
-import {createContext} from 'react';
-import { TodoContextType } from '../@types.todo';
-
-const Context = createContext<TodoContextType | []>([]);
-export default Context
--- src/components/Divider/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Divider.module.css';
-
-const Divider = () => {
-	return <div className={styles.wrapper}></div>;
-};
-
-export default Divider;
